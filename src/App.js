10a5d7dd3cf79b4bded2ab4d62fc14e9270562a0
ethmoses <moses.buta@yahoo.com.au>
2021-11-09 10:11:57 +1100

    push

--- src/App.js
+++ src/App.js
@@ -1,17 +1,62 @@
 import {
   ApolloClient,
+  // ApolloProvider,
+  // useQuery,
+  createHttpLink,
   InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
+  gql,
 } from "@apollo/client";
-import './App.css';
+import { useMutation } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import "./App.css";
 
 function App() {
-  const client = new ApolloClient({
-    uri: 'https://48p1r2roz4.sse.codesandbox.io',
-    cache: new InMemoryCache()
+  const httpLink = createHttpLink({
+    uri: "https://api.dev.fmclarity.com/graphql",
   });
+
+  const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = "eHsFWKuLrGVJR0fKRvx52dCDkAumfnUHuQCp2mTcU8n";
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  });
+
+  let assets = [];
+  const client = new ApolloClient({
+    // uri: 'https://api.dev.fmclarity.com/graphql',
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+  async function getData() {
+    let asset;
+    await client
+      .query({
+        query: gql`
+          query {
+            assets(where: { name: { contains: "PhotoSphereView" } }) {
+              name
+              locationDescription
+              gpsLat
+              gpsLng
+              _id
+              clientId
+              guId
+            }
+          }
+        `,
+      })
+      .then((result) => {
+        asset = result.data.assets;
+      });
+    return asset;
+  }
   return (
     <div className="App">
       <h1>Asset import</h1>
